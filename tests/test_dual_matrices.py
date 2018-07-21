8afc8b0f43bc7cc3d04111dfde2ec54ede6b0862
TsimpDim <dimitristsimpitas@gmail.com>
2018-07-21 19:33:12 +0300

    Added tests for get_dual_matrix

---
+++ tests/test_dual_matrices.py
@@ -0,0 +1,64 @@
+from Aplos import AplosParser, exceptions
+import os
+import pytest
+
+
+def test_get_dual_matrix_empty():
+
+    with pytest.warns(RuntimeWarning):
+        parser = AplosParser(text='')
+
+        with pytest.raises(exceptions.EmptyLPException):
+            if not parser.detect_errors():
+                A = parser.get_dual_matrix(matrix='A')
+
+def test_get_matrix_no_arg():
+    
+    FILE_NAME = 'main_lp.txt'
+    CUR_DIR = os.path.dirname(os.path.abspath(__file__))
+    test_file = os.path.join(CUR_DIR, 'files/'+FILE_NAME)
+
+    parser = AplosParser(filename=test_file)
+    if not parser.detect_errors():
+
+        with pytest.raises(exceptions.MissingArgumentsException):
+            A = parser.get_dual_matrix()
+
+def test_get_matrix_errors():
+    FILE_NAME = 'error_lp_1.txt'
+    CUR_DIR = os.path.dirname(os.path.abspath(__file__))
+    test_file = os.path.join(CUR_DIR, 'files/'+FILE_NAME)
+
+    parser = AplosParser(filename=test_file)
+    with pytest.raises(exceptions.LPErrorException):
+        A = parser.get_dual_matrix(matrix='A')
+
+def test_get_matrix():
+
+    FILE_NAME = 'main_lp.txt'
+    CUR_DIR = os.path.dirname(os.path.abspath(__file__))
+    test_file = os.path.join(CUR_DIR, 'files/'+FILE_NAME)
+
+    parser = AplosParser(filename=test_file)
+    if not parser.detect_errors():
+        A = parser.get_dual_matrix(matrix='A')
+        b = parser.get_dual_matrix(matrix='b')
+        c = parser.get_dual_matrix(matrix='c')
+        Eqin = parser.get_dual_matrix(matrix='EQin')
+        minmax = parser.get_dual_matrix(matrix='minMAX')
+        var_constr = parser.get_dual_matrix(matrix='var_constr')
+
+        expected_A = [[1,2],[2,5]]
+        expected_b = [3,2]
+        expected_c = [9,4]
+        expected_Eqin = [1,1]
+        expected_minmax = [-1]
+        expected_var_constr = [1,1]
+
+        assert A == expected_A and \
+               b == expected_b and \
+               c == expected_c and \
+               Eqin == expected_Eqin and \
+               minmax == expected_minmax and \
+               var_constr == expected_var_constr
+
