14dc52a68ac4f96e11776a9d1b6f1fc1cd412af6
TsimpDim <dimitristsimpitas@gmail.com>
2018-08-09 14:07:48 +0300

    Added read dual matrices test

--- tests/test_read_matrices.py
+++ tests/test_read_matrices.py
@@ -43,3 +43,23 @@ def test_open_file_error():
 
     with pytest.raises(exceptions.LPReadException):
         matrices = parser.read_matrices_from_file(output_file)
+
+def test_open_file_dual():
+    FILE_NAME = 'main_lp.txt'
+    CUR_DIR = os.path.dirname(os.path.abspath(__file__))
+    test_file = os.path.join(CUR_DIR, 'files/'+FILE_NAME)
+    output_file = os.path.join(CUR_DIR, 'files/output_tests/output_dual.txt')
+
+    parser = AplosParser(filename=test_file)
+    matrices = parser.read_matrices_from_file(output_file, dual=True)
+
+    ex_A = [[1,2],[2,5]]
+    ex_b = [3,2]
+    ex_c = [9,4]
+    ex_Eqin = [1, 1]
+    ex_minmax = [-1]
+    ex_varconstr = [1, 1]
+
+    ex = {'A':ex_A, 'b':ex_b, 'c':ex_c, "Eqin":ex_Eqin, 'MinMax':ex_minmax, 'VarConstr':ex_varconstr}
+
+    assert ex == matrices
